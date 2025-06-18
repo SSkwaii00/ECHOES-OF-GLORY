a77ab25807c35ccfead4e298ca692028d9567c65
Justin BieBin <baokhangbkpk@gmail.com>
2025-06-18 16:26:48 +0700

    battle up date

---
+++ Assets/khang/Script/Character/Astra.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Astra : Combatant
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        CombatantData data = ScriptableObject.CreateInstance<CombatantData>();
+        data.Name = "Astra";
+        data.HP = 90;
+        data.Attack = 14;
+        data.Agility = 70;
+        data.Element = "Ice";
+        data.Path = "Nihility";
+        data.ArtPoints = 90;
+        SetData(data);
+    }
+
+    public override int CalculateDamage(int actionIndex, Enemy target)
+    {
+        int damage = actionIndex switch
+        {
+            0 => Attack, // Attack
+            1 => (int)Mathf.RoundToInt(Attack * 2.3f), // Skill: 2.3x
+            2 => (int)Mathf.RoundToInt(Attack * 1.7f), // Util: 1.7x
+            _ => Attack
+        };
+        return damage;
+    }
+}
---
+++ Assets/khang/Script/enemy/RiftHorror.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RiftHorror : Enemy
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        EnemyData data = ScriptableObject.CreateInstance<EnemyData>();
+        data.Name = "RiftHorror";
+        data.HP = 80;
+        data.Attack = 10;
+        data.Agility = 80;
+        data.Element = "Void";
+        data.Path = "Nihility";
+        data.ArtPoints = 60;
+        SetData(data);
+    }
+
+    public override int CalculateDamage(Combatant target)
+    {
+        return Attack + (int)(Agility * 0.1f);
+    }
+}
